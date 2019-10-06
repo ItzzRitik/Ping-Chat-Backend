2e0bc66d1714defba684ccc3b912dcda5e45098c
Ritik Srivastava <ritik.space@gmail.com>
2019-10-07 01:08:41 +0530

    SocketIO Basic

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const http = require('http');
 
 const server = http.createServer(app);
 const io = socketio(server);
+const router = require('./router');
+
+app.use(router);
+
+io.on('connection', (socket) => {
+    console.log('New Connection');
+    socket.on('disconnect', () => {
+        console.log('User Left');
+    });
+});
 
 server.listen(process.env.PORT || 8080, () => {
     console.log(`Server has started`);
