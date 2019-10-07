ad7067beb28b07d15bb972594529cb98c5ad9cb4
Ritik Srivastava <ritik.space@gmail.com>
2019-10-08 00:58:38 +0530

    Setting Room Update

--- app.js
+++ app.js
@@ -19,17 +19,20 @@ io.on('connection', (socket) => {
         socket.emit('message', {user: 'admin', text: `Hi ${user.name}, Welcome to the room ${user.room}`});
         socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has Joined!`});
         socket.join(user.room);
+        io.to(user.room).emit('roomData', {room: user.room}, {users: getUsersInRoom(user.room)});
         callback();
     });
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
         io.to(user.room).emit('message',{user: user.name, text: message});
+        io.to(user.room).emit('roomData',{room: user.room, users: getUsersInRoom(user.room)});
         callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('User Left');
+        const user = removeUser(socket.id);
+        if(user) io.to(user.room).emit('message', {user: 'admin', text: `${user.name} has left.`});
     });
 });
 
