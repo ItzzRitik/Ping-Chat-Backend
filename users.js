cd4ae33bb305cc67a17febf24895123fe27c1352
Ritik Srivastava <ritik.space@gmail.com>
2019-10-08 01:49:02 +0530

    Server Complete

--- users.js
+++ users.js
@@ -5,9 +5,8 @@ const addUser = ({id, name, room}) => {
     room=room.trim().toLowerCase();
 
     const existingUser = users.find((user) => user.room === room && user.name === name);
-    if(existingUser){
-        return {error : 'Username is Taken'};
-    }
+    if(!name || !room) return { error: 'Username and room are required.' };
+    if(existingUser) return { error: 'Username is taken.' };
 
     const user = {id, name, room};
     users.push(user);
@@ -22,7 +21,6 @@ const removeUser = (id) => {
 }
 
 const getUser = (id) => users.find((user) => user.id === id);
-
-const getUsersInRoom = () => (room) => users.filter((user) => user.room === room);
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = {addUser, removeUser, getUser, getUsersInRoom};
