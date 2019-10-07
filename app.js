18c3037f08373756fcbe00e6af6e1aae949ec2fc
Ritik Srivastava <ritik.space@gmail.com>
2019-10-07 22:51:01 +0530

    Sending message

--- app.js
+++ app.js
@@ -13,8 +13,21 @@ app.use(router);
 
 io.on('connection', (socket) => {
     socket.on('join', ({name, room}, callback) => {
-        
+        const {error, user} = addUser({id: socket.id, name, room});
+        if(error) return callback(error);
+
+        socket.emit('message',{user: 'admin', text: `Hi ${user.name}, Welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has Joined!`});
+        socket.join(user.room);
+        callback();
     });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message',{user: user.name, text: message});
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('User Left');
     });
